ed20aa94bd8a01b75a39b794bc79d79a4b4acef5
Ronit Singh <code.rs2001@gmail.com>
2025-12-31 16:15:15 -0500

    Initial Commit

---
+++ FineTune/FineTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FineTuneApp.swift
+//  FineTune
+//
+//  Created by Ronit Singh on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct FineTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
